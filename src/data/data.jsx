ac54ca367a9091871b998a501c576d3f821cbec4
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-14 17:52:57 +0100

    Change verb tense for the pre-loaded activities.

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,11 +1,11 @@
 import { uid } from "uid";
 
 export const storedActivities = [
-  { id: uid(), name: "Going to a park", goodWeather: true },
-  { id: uid(), name: "Riding your bicycle", goodWeather: true },
-  { id: uid(), name: "Walking the dog", goodWeather: true },
-  { id: uid(), name: "Staying home with family", goodWeather: false },
-  { id: uid(), name: "Going to a museum", goodWeather: false },
-  { id: uid(), name: "Baking a cake", goodWeather: false },
-  { id: uid(), name: "Watching Netflix", goodWeather: false },
+  { id: uid(), name: "Go to a park", goodWeather: true },
+  { id: uid(), name: "Ride your bicycle", goodWeather: true },
+  { id: uid(), name: "Walk the dog", goodWeather: true },
+  { id: uid(), name: "Stay home with family", goodWeather: false },
+  { id: uid(), name: "Go to a museum", goodWeather: false },
+  { id: uid(), name: "Bake a cake", goodWeather: false },
+  { id: uid(), name: "Watch Netflix", goodWeather: false },
 ];
