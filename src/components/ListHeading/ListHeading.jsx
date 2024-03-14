f8fa26225a11ec7081fe17eb2f8de7c5f4c29c09
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-14 12:26:44 +0100

    Add style to the ListHeading component.

--- src/components/ListHeading/ListHeading.jsx
+++ src/components/ListHeading/ListHeading.jsx
@@ -1,13 +1,25 @@
+import "./ListHeading.css";
+
 export default function ListHeading({ weather }) {
   return (
-    <header>
-      <span>{weather.condition}</span>
-      <span>{weather.temperature} °C</span>
-      <h1>{`${
-        weather.isGoodWeather
-          ? "The weather is awesome! Go outside and:"
-          : "Bad weather outside! Here's what you can do now:"
-      }`}</h1>
+    <header className="main">
+      <div className="top-header">
+        <span className="icon">{weather.condition}</span>
+        <span className="temperature">{weather.temperature} °C</span>
+      </div>
+      <h1>
+        {weather.isGoodWeather ? (
+          <>
+            The weather is awesome! <br />
+            <span className="smaller">Go outside and...</span>
+          </>
+        ) : (
+          <>
+            Bad weather outside! <br />
+            <span className="smaller">Here&apos;s what you can do now...</span>
+          </>
+        )}
+      </h1>
     </header>
   );
 }
