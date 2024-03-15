e7ea427c07537db688083323317d7ed7035f43f2
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-15 15:29:28 +0100

    Improve the style of the InputSelect element.

--- src/components/InputSelect/InputSelect.jsx
+++ src/components/InputSelect/InputSelect.jsx
@@ -2,15 +2,12 @@ import "./InputSelect.css";
 
 export default function InputSelect({ categories }) {
   return (
-    <div>
-      <label htmlFor="category">Category</label>
-      <select name="category" id="category">
-        {categories.map((category) => (
-          <option key={category.id} value={category.id}>
-            {category.name}
-          </option>
-        ))}
-      </select>
-    </div>
+    <select name="category" id="category">
+      {categories.map((category) => (
+        <option key={category.id} value={category.id}>
+          {category.name}
+        </option>
+      ))}
+    </select>
   );
 }
